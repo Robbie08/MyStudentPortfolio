2e58ae2f851e80cf13c3348d1eefb7867fa874ba
Robert <roberinho08@gmail.com>
2018-10-31 13:47:23 -0700

    Pushing to heroku

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express(); //creates an express application
+
+//set port
+const port = process.env.PORT || 8080
+
+
+var path = require('path');
+
+app.use(express.static(__dirname));
+
+
+//handles the route 
+app.get("/", function(req,res) {
+    console.log(__dirname);
+    res.render("index")
+});
+
+
+app.listen(function () {
+    console.log("App running ")
+});
+
+app.listen(port); 
+console.log("My nigga"); 
